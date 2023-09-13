c9f41a7951b683949afb6e3c61f8a70b5df0caae
gilzoide <gilzoide@gmail.com>
2023-09-13 20:45:59 -0300

    Update sample code with object method call

--- sample/hello-gdextension-lite.c
+++ sample/hello-gdextension-lite.c
@@ -5,11 +5,34 @@ void initialize(void *userdata, GDExtensionInitializationLevel p_level) {
         return;
     }
 
-	godot_String msg = godot_new_String_from_latin1_chars("Hello from GDExtension Lite!");
-	godot_Variant msg_var = godot_Variant_from_String(&msg);
-	godot_print_v(&msg_var);
-	godot_destroy_Variant(&msg_var);
-	godot_destroy_String(&msg);
+	// print("Hello from GDExtension Lite!")
+	{
+		godot_String msg = godot_new_String_from_latin1_chars("Hello from GDExtension Lite!");
+		godot_Variant msg_var = godot_Variant_from_String(&msg);
+		godot_destroy_String(&msg);
+
+		godot_print_v(&msg_var);
+		godot_destroy_Variant(&msg_var);
+	}
+
+	// prints("OS.get_name() ==", OS.get_name())
+	{
+		godot_StringName singleton_name = godot_new_StringName_from_latin1_chars("OS");
+		godot_OS *os = godot_global_get_singleton(&singleton_name);
+		godot_destroy_StringName(&singleton_name);
+		
+		godot_String os_name = godot_OS_get_name(os);
+		godot_Variant os_name_var = godot_Variant_from_String(&os_name);
+		godot_destroy_String(&os_name);
+
+		godot_String msg = godot_new_String_from_latin1_chars("OS.get_name() ==");
+		godot_Variant msg_var = godot_Variant_from_String(&msg);
+		godot_destroy_String(&msg);
+
+		godot_prints_v(&msg_var, &os_name_var);
+		godot_destroy_Variant(&msg_var);
+		godot_destroy_Variant(&os_name_var);
+	}
 }
 
 void deinitialize(void *userdata, GDExtensionInitializationLevel p_level) {
@@ -28,3 +51,4 @@ GDExtensionBool gdextension_entry(
     r_initialization->deinitialize = &deinitialize;
     return 1;
 }
+
