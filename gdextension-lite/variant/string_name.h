cf55a7c23864e0351a158428118e08a52e4d88b4
gilzoide <gilzoide@gmail.com>
2023-10-18 08:02:31 -0300

    Add placement new from C strings for StringName

--- gdextension-lite/variant/string_name.h
+++ gdextension-lite/variant/string_name.h
@@ -4,9 +4,7 @@
 #ifndef __GDEXTENSION_LITE_STRING_NAME_H__
 #define __GDEXTENSION_LITE_STRING_NAME_H__
 
-#include <stdint.h>
-
-#include "string.h"
+#include "int.h"
 
 typedef struct godot_StringName {
 	uint8_t _[sizeof(void *)];
@@ -21,105 +19,25 @@ godot_StringName godot_new_StringName_from_utf8_chars(const char *p_contents);
 godot_StringName godot_new_StringName_from_utf16_chars(const char16_t *p_contents);
 godot_StringName godot_new_StringName_from_utf32_chars(const char32_t *p_contents);
 godot_StringName godot_new_StringName_from_wide_chars(const wchar_t *p_contents);
+void godot_placement_new_StringName_from_latin1_chars(godot_StringName *self, const char *p_contents);
+void godot_placement_new_StringName_from_utf8_chars(godot_StringName *self, const char *p_contents);
+void godot_placement_new_StringName_from_utf16_chars(godot_StringName *self, const char16_t *p_contents);
+void godot_placement_new_StringName_from_utf32_chars(godot_StringName *self, const char32_t *p_contents);
+void godot_placement_new_StringName_from_wide_chars(godot_StringName *self, const wchar_t *p_contents);
 
-godot_StringName godot_new_StringName_from_latin1_chars_and_len(const char *p_contents, GDExtensionInt p_size);
-godot_StringName godot_new_StringName_from_utf8_chars_and_len(const char *p_contents, GDExtensionInt p_size);
-godot_StringName godot_new_StringName_from_utf16_chars_and_len(const char16_t *p_contents, GDExtensionInt p_size);
-godot_StringName godot_new_StringName_from_utf32_chars_and_len(const char32_t *p_contents, GDExtensionInt p_size);
-godot_StringName godot_new_StringName_from_wide_chars_and_len(const wchar_t *p_contents, GDExtensionInt p_size);
-
-void godot_destroy_StringName(godot_StringName *self);
+godot_StringName godot_new_StringName_from_latin1_chars_and_len(const char *p_contents, godot_int p_size);
+godot_StringName godot_new_StringName_from_utf8_chars_and_len(const char *p_contents, godot_int p_size);
+godot_StringName godot_new_StringName_from_utf16_chars_and_len(const char16_t *p_contents, godot_int p_size);
+godot_StringName godot_new_StringName_from_utf32_chars_and_len(const char32_t *p_contents, godot_int p_size);
+godot_StringName godot_new_StringName_from_wide_chars_and_len(const wchar_t *p_contents, godot_int p_size);
+void godot_placement_new_StringName_from_latin1_chars_and_len(godot_StringName *self, const char *p_contents, godot_int p_size);
+void godot_placement_new_StringName_from_utf8_chars_and_len(godot_StringName *self, const char *p_contents, godot_int p_size);
+void godot_placement_new_StringName_from_utf16_chars_and_len(godot_StringName *self, const char16_t *p_contents, godot_int p_size);
+void godot_placement_new_StringName_from_utf32_chars_and_len(godot_StringName *self, const char32_t *p_contents, godot_int p_size);
+void godot_placement_new_StringName_from_wide_chars_and_len(godot_StringName *self, const wchar_t *p_contents, godot_int p_size);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif  // __GDEXTENSION_LITE_STRING_NAME_H__
-
-#ifdef GDEXTENSION_LITE_IMPLEMENTATION
-#ifndef __GDEXTENSION_LITE_STRING_NAME_H_IMPLEMENTATION__
-#define __GDEXTENSION_LITE_STRING_NAME_H_IMPLEMENTATION__
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void godot_destroy_String(godot_String *self);
-godot_StringName godot_new_StringName_from_String(const godot_String *from);
-
-godot_StringName godot_new_StringName_from_latin1_chars(const char *p_contents) {
-	godot_String str = godot_new_String_from_latin1_chars(p_contents);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_utf8_chars(const char *p_contents) {
-	godot_String str = godot_new_String_from_utf8_chars(p_contents);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_utf16_chars(const char16_t *p_contents) {
-	godot_String str = godot_new_String_from_utf16_chars(p_contents);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_utf32_chars(const char32_t *p_contents) {
-	godot_String str = godot_new_String_from_utf32_chars(p_contents);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_wide_chars(const wchar_t *p_contents) {
-	godot_String str = godot_new_String_from_wide_chars(p_contents);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_latin1_chars_and_len(const char *p_contents, GDExtensionInt p_size) {
-	godot_String str = godot_new_String_from_latin1_chars_and_len(p_contents, p_size);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_utf8_chars_and_len(const char *p_contents, GDExtensionInt p_size) {
-	godot_String str = godot_new_String_from_utf8_chars_and_len(p_contents, p_size);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_utf16_chars_and_len(const char16_t *p_contents, GDExtensionInt p_size) {
-	godot_String str = godot_new_String_from_utf16_chars_and_len(p_contents, p_size);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_utf32_chars_and_len(const char32_t *p_contents, GDExtensionInt p_size) {
-	godot_String str = godot_new_String_from_utf32_chars_and_len(p_contents, p_size);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-godot_StringName godot_new_StringName_from_wide_chars_and_len(const wchar_t *p_contents, GDExtensionInt p_size) {
-	godot_String str = godot_new_String_from_wide_chars_and_len(p_contents, p_size);
-	godot_StringName self = godot_new_StringName_from_String(&str);
-	godot_destroy_String(&str);
-	return self;
-}
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif  // __GDEXTENSION_LITE_STRING_NAME_H_IMPLEMENTATION__
-#endif  // GDEXTENSION_LITE_IMPLEMENTATION
