eaf1901daca0bcc73b8ba5823ea5ba01d0a252e6
gilzoide <gilzoide@gmail.com>
2023-10-20 20:00:21 -0300

    Fix sample code

--- sample/hello-gdextension-lite.cpp
+++ sample/hello-gdextension-lite.cpp
@@ -1,6 +1,4 @@
-#include "../gdextension-lite/gdextension-lite.hpp"
-
-using namespace godot;
+#include "../gdextension-lite/gdextension-lite.h"
 
 void initialize(void *userdata, GDExtensionInitializationLevel p_level) {
 	if (p_level != GDEXTENSION_INITIALIZATION_SCENE) {
@@ -9,32 +7,25 @@ void initialize(void *userdata, GDExtensionInitializationLevel p_level) {
 
 	// print("Hello from GDExtension Lite!")
 	{
-		godot_String msg = godot_new_String_from_latin1_chars("Hello from GDExtension Lite!");
-		godot_Variant msg_var = godot_new_Variant_from_String(&msg);
-		godot_destroy_String(&msg);
+		GDCLEANUP(godot_String) msg = godot_new_String_from_latin1_chars("Hello from GDExtension Lite!");
+		GDCLEANUP(godot_Variant) msg_var = godot_new_Variant_from_String(&msg);
 
 		godot_print(&msg_var, 0, NULL);
-		godot_destroy_Variant(&msg_var);
 	}
 
 	// prints("OS.get_name() ==", OS.get_name())
 	{
-		godot_StringName singleton_name = godot_new_StringName_from_latin1_chars("OS");
+		GDCLEANUP(godot_StringName) singleton_name = godot_new_StringName_from_latin1_chars("OS");
 		godot_OS *os = (godot_OS *) godot_global_get_singleton(&singleton_name);
-		godot_destroy_StringName(&singleton_name);
 		
-		godot_String os_name = godot_OS_get_name(os);
-		godot_Variant os_name_var = godot_new_Variant_from_String(&os_name);
-		godot_destroy_String(&os_name);
+		GDCLEANUP(godot_String) os_name = godot_OS_get_name(os);
+		GDCLEANUP(godot_Variant) os_name_var = godot_new_Variant_from_String(&os_name);
 
-		godot_String msg = godot_new_String_from_latin1_chars("OS.get_name() ==");
-		godot_Variant msg_var = godot_new_Variant_from_String(&msg);
-		godot_destroy_String(&msg);
+		GDCLEANUP(godot_String) msg = godot_new_String_from_latin1_chars("OS.get_name() ==");
+		GDCLEANUP(godot_Variant) msg_var = godot_new_Variant_from_String(&msg);
 
 		const godot_Variant *args[1] = { &os_name_var };
 		godot_prints(&msg_var, 1, args);
-		godot_destroy_Variant(&msg_var);
-		godot_destroy_Variant(&os_name_var);
 	}
 }
 
