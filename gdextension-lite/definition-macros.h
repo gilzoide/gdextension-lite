38c5d322405b9c3124dbb28b2af8a01cc5735b1f
gilzoide <gilzoide@gmail.com>
2023-10-20 17:51:19 -0300

    Add GDCLEANUP macro for automatically released Godot-typed variables

---
+++ gdextension-lite/definition-macros.h
@@ -0,0 +1,10 @@
+/** @file
+ * definition-macros.h -- Helper macros for GDExtension Lite definitions
+ */
+#ifndef __GDEXTENSION_LITE_DEFINITION_MACROS_H__
+#define __GDEXTENSION_LITE_DEFINITION_MACROS_H__
+
+#define GDCLEANUP(godot_Type) \
+	__attribute__((cleanup(godot_Type##_destroy))) godot_Type
+
+#endif
